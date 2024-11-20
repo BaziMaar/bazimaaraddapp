84bf2adef8e2c3a85fe3c98f635ea5b1b3440162
Kanishk Pareek <kanishkgit@gmail.com>
2024-11-20 15:35:29 +0530

    changes

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -77,6 +77,7 @@ function Main() {
     const checkSubscription = async (email) => {
         try {
             const response = await axios.get(`https://sattajodileak.com/payment/subscription/check?email=${email}&txn_note="Colour Trading Subscription"`);
+            
             console.log(response);
             return response.data.message === "Valid subscription found.";
         } catch (err) {
